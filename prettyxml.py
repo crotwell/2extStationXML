058d8a04a6357903cec9c6d1c9a6fc3a2fc379d9
Philip Crotwell <crotwell@seis.sc.edu>
2016-01-28 11:44:24 -0500

    add pretty print for xml

---
+++ prettyxml.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+
+import os
+import sys
+import xml.dom.minidom
+
+if (len(sys.argv) == 1):
+    print "prettyxml.py <xmlfile>"
+    sys.exit(1)
+
+if os.path.exists(sys.argv[1]):
+    F = open(sys.argv[1], 'r')
+    xml = xml.dom.minidom.parse(F)
+    print xml.toprettyxml(indent="    ")
+else:
+    print "unable to open file '%s'"%(sys.argv[1],)
+
+
