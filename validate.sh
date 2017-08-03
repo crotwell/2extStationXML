6b0812dff4f4ad194e112fa22518d431e202fa63
Philip Crotwell <crotwell@seis.sc.edu>
2017-08-03 16:55:19 -0400

    fix urls as validator tar moved, also schema file must be downloaded first, add instructions

--- validate.sh
+++ validate.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [[ ! -r 'validator/ValidateStationXml.class' ]]
+SCHEMAURL='https://anss-sis.scsn.org/xml/ext-stationxml/2.0/sis_extension.xsd'
+SCHEMA='sis_extension_2.0.xsd'
+
+if [[ ! -r 'xmlvalidator/ValidateStationXml.class' ]]
 then
     echo
-    echo "validator not in current directory, please get with:"
+    echo "xmlvalidator not in current directory, please get with:"
     echo
-    echo "wget http://maui.gps.caltech.edu/SIStrac/raw-attachment/wiki/SIS/Code/validator.tar.gz"
+    echo "wget http://wiki.anss-sis.scsn.org/SIStrac/raw-attachment/wiki/SIS/Code/validator.tar.gz"
     echo "tar ztf validator.tar.gz"
     echo
 elif [[ ! -r 'xerces-2_11_0-xml-schema-1.1-beta/xercesImpl.jar' ]]
@@ -18,6 +21,15 @@ then
     echo
     echo "Note it must be the xml-schema-1.1 version."
     echo
+elif [[ ! -r $SCHEMA ]]
+then
+    echo
+    echo "$SCHEMA not in current directory, please get with:"
+    echo
+    echo "wget -O $SCHEMA $SCHEMAURL"
+    echo
+    echo "Note you may choose the a different version, like 2.1 instead, but will need to change this script."
+    echo
 else
     if [[ ! -n $1 ]]
     then
@@ -26,7 +38,7 @@ else
     then
         echo "Warn: cannot read stationxml file"
     else
-        ERRFILE=`java -cp xerces-2_11_0-xml-schema-1.1-beta/xercesImpl.jar:xerces-2_11_0-xml-schema-1.1-beta/xml-apis.jar:xerces-2_11_0-xml-schema-1.1-beta/serializer.jar:xerces-2_11_0-xml-schema-1.1-beta/org.eclipse.wst.xml.xpath2.processor_1.1.0.jar:validator ValidateStationXml -i $*`
+        ERRFILE=`java -cp xerces-2_11_0-xml-schema-1.1-beta/xercesImpl.jar:xerces-2_11_0-xml-schema-1.1-beta/xml-apis.jar:xerces-2_11_0-xml-schema-1.1-beta/serializer.jar:xerces-2_11_0-xml-schema-1.1-beta/org.eclipse.wst.xml.xpath2.processor_1.1.0.jar:xmlvalidator ValidateStationXml -s $SCHEMA -i $*`
 
         if [ $ERRFILE = "0" ]
         then
