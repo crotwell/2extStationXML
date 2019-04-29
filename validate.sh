533d82f9835685c76abba915e8073c0c6e06b2dd
Philip Crotwell <crotwell@seis.sc.edu>
2019-04-29 15:05:03 -0400

    update to xerces 2.12.0 sis_extension_2.2

--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCHEMAURL='https://anss-sis.scsn.org/xml/ext-stationxml/2.0/sis_extension.xsd'
-SCHEMA='sis_extension_2.0.xsd'
+SCHEMAURL='https://anss-sis.scsn.org/xml/ext-stationxml/2.2/sis_extension.xsd'
+SCHEMA='sis_extension_2.2.xsd'
 
 if [[ ! -r 'xmlvalidator/ValidateStationXml.class' ]]
 then
@@ -11,13 +11,13 @@ then
     echo "wget http://wiki.anss-sis.scsn.org/SIStrac/raw-attachment/wiki/SIS/Code/validator.tar.gz"
     echo "tar ztf validator.tar.gz"
     echo
-elif [[ ! -r 'xerces-2_11_0-xml-schema-1.1-beta/xercesImpl.jar' ]]
+elif [[ ! -r 'xerces-2_12_0-xml-schema-1.1/xercesImpl.jar' ]]
 then
     echo
     echo "Xerces not in current directory, please get with:"
     echo
     echo "wget http://mirror.cc.columbia.edu/pub/software/apache/xerces/j/binaries/Xerces-J-bin.2.11.0-xml-schema-1.1-beta.tar.gz"
-    echo "tar zxf Xerces-J-bin.2.11.0-xml-schema-1.1-beta.tar.gz"
+    echo "tar zxf Xerces-J-bin.2.12.0-xml-schema-1.1.tar.gz"
     echo
     echo "Note it must be the xml-schema-1.1 version."
     echo
@@ -38,7 +38,7 @@ else
     then
         echo "Warn: cannot read stationxml file"
     else
-        ERRFILE=`java -cp xerces-2_11_0-xml-schema-1.1-beta/xercesImpl.jar:xerces-2_11_0-xml-schema-1.1-beta/xml-apis.jar:xerces-2_11_0-xml-schema-1.1-beta/serializer.jar:xerces-2_11_0-xml-schema-1.1-beta/org.eclipse.wst.xml.xpath2.processor_1.1.0.jar:xmlvalidator ValidateStationXml -s $SCHEMA -i $*`
+        ERRFILE=`java -cp xerces-2_12_0-xml-schema-1.1/xercesImpl.jar:xerces-2_12_0-xml-schema-1.1/xml-apis.jar:xerces-2_12_0-xml-schema-1.1/serializer.jar:xerces-2_12_0-xml-schema-1.1/org.eclipse.wst.xml.xpath2.processor_1.2.0.jar:xmlvalidator ValidateStationXml -s $SCHEMA -i $*`
 
         if [ $ERRFILE = "0" ]
         then
