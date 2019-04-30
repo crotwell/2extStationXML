8088a928dd5e871ae61a93f8b3e57637eea7edb3
Philip Crotwell <crotwell@seis.sc.edu>
2019-04-30 15:43:20 -0400

    fix import

--- isnrl.py
+++ isnrl.py
@@ -3,7 +3,7 @@
 check if single channel sensor response in NRL
 '''
 import checkNRL as checkNRL
-import sisxmlparser2_0 as sisxmlparser
+import sisxmlparser2_2 as sisxmlparser
 import uniqResponses as uniqResponses
 
 import argparse
