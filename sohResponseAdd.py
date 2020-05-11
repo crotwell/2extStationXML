a4f48c327ac84e70be749e1ece3060064a6e327a
Philip Crotwell <crotwell@seis.sc.edu>
2020-05-11 11:45:34 -0400

    desc on counts, no units warn us array in case additional warnings, msg on copy instPolynomial to stage, sens from polynomail stage, polynomial from polynomial stage, calc freq from instSensitivity, add decimation and stagegain optionally

--- sohResponseAdd.py
+++ sohResponseAdd.py
@@ -15,6 +15,7 @@ VERBOSE=False
 
 COUNT_UNIT = sisxmlparser.UnitsType()
 COUNT_UNIT.Name = 'count'
+COUNT_UNIT.Description = 'Digital Counts'
 
 def calcInputUnits(c):
     """gets input units for a response, checking InstrumentSensitivity,
@@ -48,7 +49,9 @@ def makeUnityResponse(c, chanCode, changes, inUnitsDict):
 
     if inputunits is None:
         if VERBOSE: print("skip chanCode %s, no units in response or units file"%(chanCode, ))
-        changes[chanCode] = "WARN: No input units"
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No input units")
         return
 
     if not hasattr(c, 'Response'):
@@ -60,7 +63,9 @@ def makeUnityResponse(c, chanCode, changes, inUnitsDict):
             resp.Stage[0].number = 1
             resp.Stage[0].Polynomial = resp.InstrumentPolynomial
             changes['numChanges'] += 1
-            changes[chanCode] = "copy InstPolynomial to Stage"
+            if not chanCode in changes:
+                changes[chanCode] = []
+            changes[chanCode].append("copy InstPolynomial to Stage")
     else:
         if not hasattr(resp, 'InstrumentSensitivity') and hasattr(resp, 'Stage'):
             if len(resp.Stage) == 1 and hasattr(resp.Stage[0], 'Polynomial'):
@@ -71,12 +76,16 @@ def makeUnityResponse(c, chanCode, changes, inUnitsDict):
                     resp.InstrumentSensitivity.InputUnits = inputunits
                     resp.InstrumentSensitivity.OutputUnits = resp.Stage[0].Polynomial.OutputUnits
                     changes['numChanges'] += 1
-                    changes[chanCode] = "sensitivity from polynomial stage"
+                    if not chanCode in changes:
+                        changes[chanCode] = []
+                    changes[chanCode].append("sensitivity from polynomial stage")
                 else:
                     # nonlinear, copy to InstPolynomial
                     resp.InstrumentPolynomial = resp.Stage[0].Polynomial
                     changes['numChanges'] += 1
-                    changes[chanCode] = "inst polynomial from polynomial stage"
+                    if not chanCode in changes:
+                        changes[chanCode] = []
+                    changes[chanCode].append("inst polynomial from polynomial stage")
         else:
             if not hasattr(resp, 'InstrumentSensitivity'):
                 resp.InstrumentSensitivity = sisxmlparser.SensitivityType()
@@ -105,19 +114,141 @@ def makeUnityResponse(c, chanCode, changes, inUnitsDict):
                 resp.Stage[0].Decimation.Correction = 0.0
                 resp.Stage[0].StageGain = sisxmlparser.GainType()
                 resp.Stage[0].StageGain.Value = 1.0
-                resp.Stage[0].StageGain.Frequency = 0.0
+                resp.Stage[0].StageGain.Frequency = resp.InstrumentSensitivity.Frequency
                 changes['numChanges'] += 1
-                changes[chanCode] = "unity Stage"
+                if not chanCode in changes:
+                    changes[chanCode] = []
+                changes[chanCode].append("unity Stage")
 
+def makeUnityStageGain(c, chanCode, changes):
+    inputunits = calcInputUnits(c)
+    if inputunits is None and c.code in list(inUnitsDict):
+        inputunits = sisxmlparser.UnitsType()
+        inputunits.Name = inUnitsDict[c.code]
 
-def addSohResponse(staxml, inunitsDict):
-    changes = { 'numChanges': 0 }
+    if inputunits is None:
+        if VERBOSE: print("skip chanCode %s, no units in response or units file"%(chanCode, ))
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No input units")
+        return
+
+    if not hasattr(c, 'Response'):
+        if VERBOSE: print("skip chanCode %s, no response"%(chanCode, ))
+
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No response")
+        return
+    resp = c.Response
+    if not hasattr(resp, 'Stage'):
+        if VERBOSE: print("skip chanCode %s, no stage in response"%(chanCode, ))
+
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No stage in response")
+        return
+    if not hasattr(resp.Stage[0], 'StageGain') \
+    and not hasattr(resp.Stage[0], 'Decimation') \
+    and hasattr(resp.Stage[0], 'PolesZeros'):
+        gainFreq = 0.0
+        if hasattr(resp.Stage[0].PolesZeros, 'NormalizationFrequency'):
+            gainFreq = resp.Stage[0].PolesZeros.NormalizationFrequency
+        elif hasattr(resp.InstrumentSensitivity, 'Frequency'):
+            gainFreq = resp.InstrumentSensitivity.Frequency
+        overallGain = resp.InstrumentSensitivity.Value
+        for s in resp.Stage:
+            if hasattr(s, 'StageGain'):
+                overallGain = overallGain / s.StageGain.Value
+        resp.Stage[0].StageGain = sisxmlparser.GainType()
+        resp.Stage[0].StageGain.Value = overallGain
+        resp.Stage[0].StageGain.Frequency = gainFreq
+        changes['numChanges'] += 1
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("stage 0 add StageGain")
+
+def makeDecimationStage(c, chanCode, changes):
+    if not hasattr(c, 'Response'):
+        if VERBOSE: print("skip chanCode %s, no response"%(chanCode, ))
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No response")
+        return
+    resp = c.Response
+    if not hasattr(resp, 'Stage'):
+        if VERBOSE: print("skip chanCode %s, no stage in response"%(chanCode, ))
+
+        if not chanCode in changes:
+            changes[chanCode] = []
+        changes[chanCode].append("WARN: No stage in response")
+        return
+    for stage in resp.Stage:
+        if hasattr(stage, 'Decimation'):
+            # if Decimation exists in some stage, nothing needed
+            return
+    lastStage = resp.Stage[len(resp.Stage)-1]
+    if hasattr(lastStage, 'PolesZeros'):
+        inUnits = lastStage.PolesZeros.OutputUnits
+    elif hasattr(lastStage, 'Coefficients'):
+        inUnits = lastStage.Coefficients.OutputUnits
+    elif hasattr(lastStage, 'FIR'):
+        inUnits = lastStage.FIR.OutputUnits
+    elif hasattr(lastStage, 'ResponseList'):
+        inUnits = lastStage.ResponseList.OutputUnits
+    else:
+        inUnits = COUNT_UNIT
+    stage = sisxmlparser.ResponseStageType()
+    resp.Stage.append(stage)
+    stage.number = len(resp.Stage)
+    stage.Coefficients = sisxmlparser.CoefficientsType()
+    stage.Coefficients.InputUnits = inUnits
+    stage.Coefficients.OutputUnits = sisxmlparser.UnitsType()
+    stage.Coefficients.OutputUnits = COUNT_UNIT
+    stage.Coefficients.CfTransferFunctionType = 'DIGITAL'
+    stage.Decimation = sisxmlparser.DecimationType()
+    stage.Decimation.InputSampleRate = c.SampleRate
+    stage.Decimation.Factor = 1
+    stage.Decimation.Offset = 0
+    stage.Decimation.Delay = 0.0
+    stage.Decimation.Correction = 0.0
+    stage.StageGain = sisxmlparser.GainType()
+    stage.StageGain.Value = 1.0
+    stage.StageGain.Frequency = 0.0
+    if hasattr(resp.Stage[0].PolesZeros, 'NormalizationFrequency'):
+        stage.StageGain.Frequency = resp.Stage[0].PolesZeros.NormalizationFrequency
+    elif hasattr(resp.InstrumentSensitivity, 'Frequency'):
+        stage.StageGain.Frequency = resp.InstrumentSensitivity.Frequency
+    changes['numChanges'] += 1
+    if not chanCode in changes:
+        changes[chanCode] = []
+    changes[chanCode].append("unity Decimation Stage")
+
+def addSohResponse(staxml, inunitsDict, changes):
     for n in staxml.Network:
       for s in n.Station:
         for c in s.Channel:
           chanCode = checkNRL.getChanCodeId(n, s, c)
           if VERBOSE: print("soh response chanCode %s "%(chanCode, ))
           makeUnityResponse(c, chanCode, changes, inunitsDict)
+    return changes
+
+def addStageGain(staxml, changes):
+    for n in staxml.Network:
+      for s in n.Station:
+        for c in s.Channel:
+          chanCode = checkNRL.getChanCodeId(n, s, c)
+          if VERBOSE: print("stageGain response chanCode %s "%(chanCode, ))
+          makeUnityStageGain(c, chanCode, changes)
+    return changes
+
+def addDecimationStage(staxml, changes):
+    for n in staxml.Network:
+      for s in n.Station:
+        for c in s.Channel:
+          chanCode = checkNRL.getChanCodeId(n, s, c)
+          if VERBOSE: print("stageGain response chanCode %s "%(chanCode, ))
+          makeDecimationStage(c, chanCode, changes)
     return changes
 
 def parseUnitsFile(unitsFile):
@@ -133,6 +264,8 @@ def initArgParser():
   parser = argparse.ArgumentParser(description='Add dummy responses for SOH channels in StationXML.')
   parser.add_argument('-s', '--stationxml', required=True, help="input FDSN StationXML file, often retrieved from http://service.iris.edu/fdsnws/station/1/")
   parser.add_argument('-u', '--units', type=argparse.FileType('r'), help="channel input units file, lines like 'VMU,VMV,VMW volt'")
+  parser.add_argument('-g', '--gainstage', action="store_true", help="add unity stage gain to polezero stage without a gain")
+  parser.add_argument('-d', '--decimationstage', action="store_true", help="add unity decimation stage gain to end of stages for response without a decimation")
   parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
   parser.add_argument('-v', '--verbose', action='store_true', help="verbose output")
   return parser.parse_args()
@@ -157,8 +290,13 @@ def main():
     if parseArgs.units:
         inunits = parseUnitsFile(parseArgs.units)
 
+    changes = { 'numChanges': 0 }
     staxml = sisxmlparser.parse(parseArgs.stationxml)
-    changes = addSohResponse(staxml, inunits)
+    addSohResponse(staxml, inunits, changes)
+    if parseArgs.gainstage:
+        addStageGain(staxml, changes)
+    if parseArgs.decimationstage:
+        addDecimationStage(staxml, changes)
     print("ok (%d changes)"%(changes['numChanges'],))
     if VERBOSE:
         for k, v in changes.items():
